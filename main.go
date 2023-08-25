dbd9b196155e123d21063bd849d7a4c8440c5823
Blagovest Petrov <blagovest@petrovs.info>
2023-08-25 21:13:42 +0300

    Added GPL header

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+/*
+***********************************************
+
+	This file is part of Resticara.
+
+Resticara is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Resticara is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+(c)2023 Vute Tech Ltd. <office@vute.tech>
+(c)2023 Blagovest Petrov <blagovest@petrovs.info>
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+*/
 package main
 
 import (
